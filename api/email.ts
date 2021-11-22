a64938ec638f5f5d8311483386c9e1dde97f988e
Volodymyr Komarov <vovikems@gmail.com>
2021-11-23 01:51:19 +0200

    Update API endpoint to confirm email

--- api/email.ts
+++ api/email.ts
@@ -1,5 +1,5 @@
 import { NuxtAxiosInstance } from '@nuxtjs/axios'
 
 export function confirmEmail(http: NuxtAxiosInstance, token: string) {
-    return http.$post(`/api/auth/email/confirmation/${token}`)
+    return http.$post(`/api/auth/email/confirmation/confirm/${token}`)
 }
